8b6ea4e85e6e8fccb932fbaf51478e4e98cb2dd1
alex <alex.getafe11@gmail.com>
2021-09-21 19:02:39 +0200

    Alex 21/09/2021 19:02;  -He incluido el enunciado

---
+++ Ejercicio11/src/paquete/Ejecuta.java
@@ -0,0 +1,14 @@
+package paquete;
+
+public class Ejecuta {
+
+	public static void main(String[] args) {
+
+			/*
+			 11. Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
+			como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
+			el nombre del alumno
+			  */
+	}
+
+}
