caad39dcf45f7b359ebc553b22a1c20daa191b90
alex <alex.getafe11@gmail.com>
2021-09-22 12:10:08 +0200

    Ejercicio sin boolean

---
+++ Ejercicio11/src/paquete/Ejecuta2.java
@@ -0,0 +1,61 @@
+package paquete;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Ejecuta2 {
+
+	public static void main(String[] args) {
+
+			/*
+				Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
+				como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
+				el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de pedir el
+				año de nacimiento. SIN BOOLEAN
+			 */
+		
+
+		String nombre, nombreMenor= "";
+		int anoNacimiento;
+		int anoActual;
+		int edad;
+		int contEdad=0;
+		int menor=Integer.MAX_VALUE;
+		int suma=0;
+		float media=0;
+	
+		Scanner sc= new Scanner(System.in);
+		//Calendar calendario = Calendar.getInstance();
+		anoActual= Calendar.getInstance().get(Calendar.YEAR);
+		System.out.println(anoActual);
+		
+		
+		System.out.println("Introduce nombre");
+		nombre= sc.nextLine();
+
+		while(nombre.equals("fin") == false) {
+		 
+	
+			System.out.println("Introduce año de nacimiento");
+			//Quiero convertir a INT, como no puedo usar int , utilizo el ENVOLVENTE (la clase ) Integer.valueOF
+			anoNacimiento= Integer.valueOf(sc.nextLine());
+
+			edad= anoActual - anoNacimiento;
+			
+			contEdad++;
+			suma += edad ;
+			
+			if(edad < menor) {
+			menor = edad;
+			nombreMenor= nombre;
+			}
+		}
+			
+		sc.close();
+		
+		if(contEdad > 0) {
+		media= (float)suma/contEdad;		
+		System.out.println("La edad media de los alumno es : " + media + " y el alumno menor es: " + nombreMenor + " que tiene " + menor);
+		}
+	}
+}
